3dba6ae87b71050adf52339193b85cdcbf451112
Kerstin Farke <kerstin.farke@hp.com>
2015-09-27 00:50:36 +0200

    new variables for firstname, lastname, email, password, team, including their setters and getters (in domain --> User)

--- meetmeserver/src/main/java/de/dhbw/meetme/domain/User.java
+++ meetmeserver/src/main/java/de/dhbw/meetme/domain/User.java
@@ -14,8 +14,53 @@ import javax.xml.bind.annotation.XmlRootElement;
 public class User extends PersistentObject {
     //nicht vergessen: alle Funktionen aus Persistent Object werden übernommen
   private String name;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+    private String team;
 
-  public String getName() {
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
+
+    public String getName() {
     return name;
   }
 
