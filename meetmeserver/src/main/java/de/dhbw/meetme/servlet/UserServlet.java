d5afa8792f8e7daa517abdc9042c98fb58b2f3d3
Paul Neustadt <paul.neustadt@googlemail.com>
2015-10-01 13:06:22 +0200

    leitet auf Conf Page

--- meetmeserver/src/main/java/de/dhbw/meetme/servlet/UserServlet.java
+++ meetmeserver/src/main/java/de/dhbw/meetme/servlet/UserServlet.java
@@ -42,7 +42,7 @@ public class UserServlet extends HttpServlet {
     User user = new User();
     user.setName(request.getParameter("username"));
     user.setFirstname(request.getParameter("name"));
-    user.setLastname(request.getParameter ("lastname"));
+    user.setLastname(request.getParameter("lastname"));
     user.setEmail(request.getParameter("e-mail"));
     user.setPassword(request.getParameter("password"));
     //user.chooseTeam();
@@ -58,15 +58,77 @@ public class UserServlet extends HttpServlet {
     users.add(user);
 
 
+    //response.sendRedirect("http://localhost:63342/GetToThePoint/meetmeserver/src/main/webapp/confirmation.html");
+
     response.setContentType("text/html");
     response.setBufferSize(8192);
     try (PrintWriter out = response.getWriter()) {
       out.println("<html lang=\"en\"><head><title>Servlet Hello</title></head>");
 
       // then write the data of the response
-      out.println("<body  bgcolor=\"#ffffff\">"
-          + "<h2>Known users:</h2>");
 
+      out.println("<!doctype html>\n" +
+              "<html>\n" +
+              "<head>\n" +
+              "<meta charset=\"utf-8\">\n" +
+              "<title>GetToThePoint!</title>\n" +
+              "<link rel=\"stylesheet\" href=\"style/style.css\">\n" +
+              "\n" +
+              "<!---VALIDATION JAVASCRIPT---->\n" +
+              "\n" +
+              "<script>\n" +
+              "function validateForm() {\n" +
+              "    var x = document.forms[\"registration\"][\"name\"][\"lastname\"][\"username\"][\"e-mail\"][\"confirm_mail\"][\"password\"][\"confirm_password\"].value;\n" +
+              "\tif ( [\"username\"] != element.defaultValue) {\n" +
+              "\t\t\n" +
+              "\t} else if ( ([\"e-mail\"] != [\"confirm_mail\"]) || ([\"password\"] != [\"confirm_password\"])  ) {\n" +
+              "\t \talert(\"E-Mail Address doesn't match!\");\n" +
+              "\t }\n" +
+              "\telse {\t\n" +
+              "\t\tif (x == null || x == \"\")  {\n" +
+              "        \talert(\"All fields must be filled out.\");\n" +
+              "        \treturn false;\n" +
+              "\t\t\t\t}\n" +
+              "\t}\n" +
+              "}\n" +
+              "</script>\n" +
+              "\n" +
+              "</head>\n" +
+              "<body>\n" +
+              "<header><center><img src=\"img/header.gif\"></center></header>\n" +
+              "<section id=\"content\">\n" +
+              "<h1>Gratulation!</h1>\n" +
+              "<p>Herzlichen Glückwunsch du hast dich erfolgreich registriert. <br />\n" +
+              "Verschwende nun keinen Moment! Schalte dein GPS-Signal an und es kann losgehen. <br />\n" +
+              "\n" +
+              " \n" +
+              " <a href=\"index.html\">Kehre zurück zur Startseite </a> </p>\n" +
+              "\n" +
+              "\n" +
+              "\n" +
+              "</section>\n" +
+              "\n" +
+              "\n" +
+              "\n" +
+              "\n" +
+              "<footer>\n" +
+              "<div class=\"innner\">\n" +
+              "<p id=\"app\">Available for <br />\n" +
+              "<a href=\"http://play.google.com\" target=\"new\"><img src=\"img/google.png\" width=\"55%\"></a>\n" +
+              "</p>\n" +
+              "<div id=\"social_icons\"><img style=\"float:right;\"src=\"img/social_media.PNG\"></div>\n" +
+              "\n" +
+              "<p id=\"legal\">Copyright (C) 2015</p>\n" +
+              "</div>\n" +
+              "\n" +
+              "</footer>\n" +
+              "\n" +
+              "</body>\n" +
+              "\n" +
+              "</html>\n");
+      /*out.println("<body  bgcolor=\"#ffffff\">"
+          + "<h1>Vielen Dank für die Registrierung, "+request.getParameter("username")+"!"
+          + "<h2>Known users:</h2>");
       for(User u: users) {
         out.println(u + "<br/>");
       }
@@ -80,7 +142,7 @@ public class UserServlet extends HttpServlet {
           dispatcher.include(request, response);
         }
       }
-      out.println("</body></html>");
+      out.println("</body></html>");*/
     }
   }
 
