a3bd746cb8d80bf1e686771585ddcfc783dc5536
Paul Neustadt <paul.neustadt@googlemail.com>
2015-10-16 10:57:20 +0200

    nimmt wieder Weiterleitung von Website auf. Prüfung auf usernameUnique ist auskommentiert

--- meetmeserver/src/main/java/de/dhbw/meetme/servlet/UserServlet.java
+++ meetmeserver/src/main/java/de/dhbw/meetme/servlet/UserServlet.java
@@ -33,97 +33,159 @@ public class UserServlet extends HttpServlet {
 
 
   @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    log.debug("UserServlet get");
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      log.debug("UserServlet get");
 
-    transaction.begin();
-    Collection<User> users = userClassicDao.list();
+      transaction.begin();
+      Collection<User> users = userClassicDao.list();
 
-      //todo Paul: überprüfen ob Username vergeben
-    User user = new User();
-    user.setName(request.getParameter("username"));
-    user.setFirstname(request.getParameter("name"));
-    user.setLastname(request.getParameter("lastname"));
-    user.setEmail(request.getParameter("e-mail"));
-    user.setPassword(request.getParameter("password"));
-    user.setTeam(request.getParameter("teams"));
-      //user.chooseTeam(); für zufällige Zuordnung der Teams
+      //prüft, ob Username unique
+      /*if (!UserClassicDao.usernameUnique(request.getParameter("username"))) {
+          transaction.commit();
+          //gibt Error Page zurück
+          out.println("<!doctype html>\n" +
+                  "<html>\n" +
+                  "<head>\n" +
+                  "<meta charset=\"utf-8\">\n" +
+                  "<title>GetToThePoint!</title>\n" +
+                  "<link rel=\"stylesheet\" href=\"style/style.css\">\n" +
+                  "\n" +
+                  "<!---VALIDATION JAVASCRIPT---->\n" +
+                  "\n" +
+                  "<script>\n" +
+                  "function validateForm() {\n" +
+                  "    var x = document.forms[\"registration\"][\"name\"][\"lastname\"][\"username\"][\"e-mail\"][\"confirm_mail\"][\"password\"][\"confirm_password\"].value;\n" +
+                  "\tif ( [\"username\"] != element.defaultValue) {\n" +
+                  "\t\t\n" +
+                  "\t} else if ( ([\"e-mail\"] != [\"confirm_mail\"]) || ([\"password\"] != [\"confirm_password\"])  ) {\n" +
+                  "\t \talert(\"E-Mail Address doesn't match!\");\n" +
+                  "\t }\n" +
+                  "\telse {\t\n" +
+                  "\t\tif (x == null || x == \"\")  {\n" +
+                  "        \talert(\"All fields must be filled out.\");\n" +
+                  "        \treturn false;\n" +
+                  "\t\t\t\t}\n" +
+                  "\t}\n" +
+                  "}\n" +
+                  "</script>\n" +
+                  "\n" +
+                  "</head>\n" +
+                  "<body>\n" +
+                  "<header><center><img src=\"img/header.gif\"></center></header>\n" +
+                  "<section id=\"content\">\n" +
+                  "<h1>Entschuldigung!</h1>\n" +
+                  "<p>Es ist ein Fehler bei deiner Registrierung aufgetreten. <br />\n" +
+                  "Wahrscheinlich ist dein Username bereits vergeben Bitte suche dir einen anderen Namen aus und versuche es noch einmal. <br />\n" +
+                  "\n" +
+                  " \n" +
+                  " <a href=\"index.html\">Kehre zurück zur Startseite </a> </p>\n" +
+                  "\n" +
+                  "\n" +
+                  "\n" +
+                  "</section>\n" +
+                  "\n" +
+                  "\n" +
+                  "\n" +
+                  "\n" +
+                  "<footer>\n" +
+                  "<div class=\"innner\">\n" +
+                  "<p id=\"app\">Available for <br />\n" +
+                  "<a href=\"http://play.google.com\" target=\"new\"><img src=\"img/google.png\" width=\"55%\"></a>\n" +
+                  "</p>\n" +
+                  "<div id=\"social_icons\"><img style=\"float:right;\"src=\"img/social_media.PNG\"></div>\n" +
+                  "\n" +
+                  "<p id=\"legal\">Copyright (C) 2015</p>\n" +
+                  "</div>\n" +
+                  "\n" +
+                  "</footer>\n" +
+                  "\n" +
+                  "</body>\n" +
+                  "\n" +
+                  "</html>\n");
+      } else {*/
+          User user = new User();
+          user.setName(request.getParameter("username"));
+          user.setFirstname(request.getParameter("name"));
+          user.setLastname(request.getParameter("lastname"));
+          user.setEmail(request.getParameter("e-mail"));
+          user.setPassword(request.getParameter("password"));
+          user.setTeam(request.getParameter("teams"));
 
-    userClassicDao.persist(user);
-    transaction.commit();
+          userClassicDao.persist(user);
+          transaction.commit();
 
-    users = new ArrayList<>(users); // cloning the read-only list so that we can add something
-    users.add(user);
+          users = new ArrayList<>(users); // cloning the read-only list so that we can add something
+          users.add(user);
 
 
-    //response.sendRedirect("http://localhost:63342/GetToThePoint/meetmeserver/src/main/webapp/confirmation.html");
+          //response.sendRedirect("http://localhost:63342/GetToThePoint/meetmeserver/src/main/webapp/confirmation.html");
 
-    response.setContentType("text/html");
-    response.setBufferSize(8192);
-    try (PrintWriter out = response.getWriter()) {
-      out.println("<html lang=\"en\"><head><title>Servlet Hello</title></head>");
+          response.setContentType("text/html");
+          response.setBufferSize(8192);
+          try (PrintWriter out = response.getWriter()) {
+              out.println("<html lang=\"en\"><head><title>Servlet Hello</title></head>");
 
-      // then write the data of the response
+              // then write the data of the response
 
-      out.println("<!doctype html>\n" +
-              "<html>\n" +
-              "<head>\n" +
-              "<meta charset=\"utf-8\">\n" +
-              "<title>GetToThePoint!</title>\n" +
-              "<link rel=\"stylesheet\" href=\"style/style.css\">\n" +
-              "\n" +
-              "<!---VALIDATION JAVASCRIPT---->\n" +
-              "\n" +
-              "<script>\n" +
-              "function validateForm() {\n" +
-              "    var x = document.forms[\"registration\"][\"name\"][\"lastname\"][\"username\"][\"e-mail\"][\"confirm_mail\"][\"password\"][\"confirm_password\"].value;\n" +
-              "\tif ( [\"username\"] != element.defaultValue) {\n" +
-              "\t\t\n" +
-              "\t} else if ( ([\"e-mail\"] != [\"confirm_mail\"]) || ([\"password\"] != [\"confirm_password\"])  ) {\n" +
-              "\t \talert(\"E-Mail Address doesn't match!\");\n" +
-              "\t }\n" +
-              "\telse {\t\n" +
-              "\t\tif (x == null || x == \"\")  {\n" +
-              "        \talert(\"All fields must be filled out.\");\n" +
-              "        \treturn false;\n" +
-              "\t\t\t\t}\n" +
-              "\t}\n" +
-              "}\n" +
-              "</script>\n" +
-              "\n" +
-              "</head>\n" +
-              "<body>\n" +
-              "<header><center><img src=\"img/header.gif\"></center></header>\n" +
-              "<section id=\"content\">\n" +
-              "<h1>Gratulation!</h1>\n" +
-              "<p>Herzlichen Glückwunsch du hast dich erfolgreich registriert. <br />\n" +
-              "Verschwende nun keinen Moment! Schalte dein GPS-Signal an und es kann losgehen. <br />\n" +
-              "\n" +
-              " \n" +
-              " <a href=\"index.html\">Kehre zurück zur Startseite </a> </p>\n" +
-              "\n" +
-              "\n" +
-              "\n" +
-              "</section>\n" +
-              "\n" +
-              "\n" +
-              "\n" +
-              "\n" +
-              "<footer>\n" +
-              "<div class=\"innner\">\n" +
-              "<p id=\"app\">Available for <br />\n" +
-              "<a href=\"http://play.google.com\" target=\"new\"><img src=\"img/google.png\" width=\"55%\"></a>\n" +
-              "</p>\n" +
-              "<div id=\"social_icons\"><img style=\"float:right;\"src=\"img/social_media.PNG\"></div>\n" +
-              "\n" +
-              "<p id=\"legal\">Copyright (C) 2015</p>\n" +
-              "</div>\n" +
-              "\n" +
-              "</footer>\n" +
-              "\n" +
-              "</body>\n" +
-              "\n" +
-              "</html>\n");
+              out.println("<!doctype html>\n" +
+                      "<html>\n" +
+                      "<head>\n" +
+                      "<meta charset=\"utf-8\">\n" +
+                      "<title>GetToThePoint!</title>\n" +
+                      "<link rel=\"stylesheet\" href=\"style/style.css\">\n" +
+                      "\n" +
+                      "<!---VALIDATION JAVASCRIPT---->\n" +
+                      "\n" +
+                      "<script>\n" +
+                      "function validateForm() {\n" +
+                      "    var x = document.forms[\"registration\"][\"name\"][\"lastname\"][\"username\"][\"e-mail\"][\"confirm_mail\"][\"password\"][\"confirm_password\"].value;\n" +
+                      "\tif ( [\"username\"] != element.defaultValue) {\n" +
+                      "\t\t\n" +
+                      "\t} else if ( ([\"e-mail\"] != [\"confirm_mail\"]) || ([\"password\"] != [\"confirm_password\"])  ) {\n" +
+                      "\t \talert(\"E-Mail Address doesn't match!\");\n" +
+                      "\t }\n" +
+                      "\telse {\t\n" +
+                      "\t\tif (x == null || x == \"\")  {\n" +
+                      "        \talert(\"All fields must be filled out.\");\n" +
+                      "        \treturn false;\n" +
+                      "\t\t\t\t}\n" +
+                      "\t}\n" +
+                      "}\n" +
+                      "</script>\n" +
+                      "\n" +
+                      "</head>\n" +
+                      "<body>\n" +
+                      "<header><center><img src=\"img/header.gif\"></center></header>\n" +
+                      "<section id=\"content\">\n" +
+                      "<h1>Gratulation!</h1>\n" +
+                      "<p>Herzlichen Glückwunsch du hast dich erfolgreich registriert. <br />\n" +
+                      "Verschwende nun keinen Moment! Schalte dein GPS-Signal an und es kann losgehen. <br />\n" +
+                      "\n" +
+                      " \n" +
+                      " <a href=\"index.html\">Kehre zurück zur Startseite </a> </p>\n" +
+                      "\n" +
+                      "\n" +
+                      "\n" +
+                      "</section>\n" +
+                      "\n" +
+                      "\n" +
+                      "\n" +
+                      "\n" +
+                      "<footer>\n" +
+                      "<div class=\"innner\">\n" +
+                      "<p id=\"app\">Available for <br />\n" +
+                      "<a href=\"http://play.google.com\" target=\"new\"><img src=\"img/google.png\" width=\"55%\"></a>\n" +
+                      "</p>\n" +
+                      "<div id=\"social_icons\"><img style=\"float:right;\"src=\"img/social_media.PNG\"></div>\n" +
+                      "\n" +
+                      "<p id=\"legal\">Copyright (C) 2015</p>\n" +
+                      "</div>\n" +
+                      "\n" +
+                      "</footer>\n" +
+                      "\n" +
+                      "</body>\n" +
+                      "\n" +
+                      "</html>\n");
       /*out.println("<body  bgcolor=\"#ffffff\">"
           + "<h1>Vielen Dank für die Registrierung, "+request.getParameter("username")+"!"
           + "<h2>Known users:</h2>");
@@ -141,7 +203,9 @@ public class UserServlet extends HttpServlet {
         }
       }
       out.println("</body></html>");*/
-    }
+
+          }
+      }
   }
 
-}
+//}
